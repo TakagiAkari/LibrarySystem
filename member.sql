4bca4ed6214c15e133736bb3d2758605c5f96b07
Rina Hosokubo <2021java52-student020@la-study.com>
2021-06-16 15:33:06 +0900

    member.sql情報追加しました

--- member.sql
+++ member.sql
@@ -1,7 +1,8 @@
+DROP TABLE IF EXISTS lending;
 DROP TABLE IF EXISTS member;
 
 CREATE TABLE member(
-  user_id INTEGER PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   user_name TEXT NOT NULL,
   address TEXT NOT NULL,
   tel TEXT NOT NULL,
@@ -9,5 +10,9 @@ CREATE TABLE member(
   enter_day DATE NOT NULL,
   leave_day DATE
 );
+
+INSERT INTO member(user_name, address, tel, email, enter_day) VALUES('—é–Ø ˜a•v', '“Œ‹“sa’J‹æ','111-1111', '1234@email.com', '20210401');
+INSERT INTO member(user_name, address, tel, email, enter_day) VALUES('“c’† ^‹|', '“Œ‹“s¢“c’J‹æ','222-2222', '5656@email.com', '20200503');
+INSERT INTO member(user_name, address, tel, email, enter_day, leave_day) VALUES('“n•Ó _“ñ', '“Œ‹“s`‹æ','333-3333', '2929@email.com', '20210520', '20210601');
 
 ALTER TABLE member OWNER TO admin;
