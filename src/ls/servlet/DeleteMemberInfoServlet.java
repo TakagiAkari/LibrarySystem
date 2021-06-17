51228a59c9f41d321f9188b1790d6c89db0b7876
Takagi Akari <2021java52-student009@la-study.com>
2021-06-17 13:26:12 +0900

    途中です！確認よろしくお願いします。

--- src/ls/servlet/DeleteMemberInfoServlet.java
+++ src/ls/servlet/DeleteMemberInfoServlet.java
@@ -1,54 +1,39 @@
 package ls.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteMenberInfoServlet
- */
-@WebServlet("/DeleteMemberInfoServlet")
+import ls.dao.DAOException;
+import ls.dao.MemberDAO;
+
 public class DeleteMemberInfoServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteMemberInfoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)
+	throws ServletException, IOException {
 		String error = "";
 		try {
 			request.setCharacterEncoding("UTF-8");
+			String action = request.getParameter("acttion");
 			//パラメーターの取得
-			String userId = request.getParameter("userId");
+			String userID = request.getParameter("userID");
 			//DAOが入る
-
+			MemberDAO memDao = new MemberDAO();
 			//削除メソッドを呼び出す
-			int count = dao.delete(userId)
+			boolean count = memDao.ExistsUserID(userID);
 			//削除するものをリクエストスコープに入れる
 			request.setAttribute("count", count);
-
 			//例外処理
-		}catch (IllegalStateException e) {
-			error ="エラーです";
-		}catch (Exception e) {
-			error ="エラーです";
+		}catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました。");
+			//gotoPage(request, response, "/errInternal.jsp");
 		}finally {
 			request.setAttribute("error", error);
-			request.getRequestDispatcher("/LibrarySystem/complete").forward(request, response);
+			request.getRequestDispatcher("/LibrarySystem/DeleteComplete").forward(request, response);
 		}
 	}
 
