6d44a4ef0c767b994d6d18fba51b5d2da558bcfa
Takagi Akari <2021java52-student009@la-study.com>
2021-06-17 13:29:52 +0900

    DAOも途中です！確認よろしくお願いします。

--- src/ls/dao/MemberDAO.java
+++ src/ls/dao/MemberDAO.java
@@ -1,22 +1,21 @@
 package ls.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
 
 public class MemberDAO {
 
 	private Connection con;
 
-
 	public MemberDAO() throws DAOException{
 		getConnetion();
 	}
 
-
 	private void getConnetion() throws DAOException{
-
 		try {
 			Class.forName("org.postgresql.Driver");
 			String url ="jdbc:postgresql:library_system";
@@ -34,4 +33,46 @@ public class MemberDAO {
 			con = null;
 		}
 	}
+
+
+
+		public boolean ExistsUserID(String userID) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		 try {
+			//SQL
+			String sql = "SELECT * FROM member WHERE user_id = ?";
+			st = con.prepareStatement(sql);
+			//SQLをデータベースへ
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+
+				Date leaveDay = rs.getDate("leaveDay");
+
+			} else {
+				return false;
+			}
+		 }
+		 catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(st != null) st.close();
+				close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+
+	}
 }
+
+
+
+
+
