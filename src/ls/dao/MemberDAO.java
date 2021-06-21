6ef2ae51e5aca7171f24cbc6ad308f37a65fb494
Takagi Akari <2021java52-student009@la-study.com>
2021-06-21 14:10:41 +0900

    add MemberDAO

--- src/ls/dao/MemberDAO.java
+++ src/ls/dao/MemberDAO.java
@@ -14,95 +14,10 @@ public class MemberDAO {
 	private Connection con;
 
 	public MemberDAO() throws DAOException{
-		getConnection();
+		getConnetion();
 	}
 
-//Dateはjava.sql.Date
-	public int addMember(String name, String address, String tel, String email, Date birth, Date enterDaysql) throws DAOException {
-		if(con == null) {
-			getConnection();
-		}
-		PreparedStatement st = null;
-		try {
-			String sql = "INSERT INTO member(user_name, address, tel, email, birthday, enter_day) VALUES(?, ?, ?, ?, ?, ?)";
-			st = con.prepareStatement(sql);
-			st.setString(1, name);
-			st.setString(2, address);
-			st.setString(3, tel);
-			st.setString(4, email);
-			st.setDate(5, birth);
-			st.setDate(6, enterDaysql);
-
-			int rows = st.executeUpdate();
-			return rows;
-		}catch(Exception e) {
-			e.printStackTrace();
-			throw new DAOException("会員情報の新規登録に失敗しました");
-		}finally {
-			try {
-				if(st != null) {
-					st.close();
-					close();
-					}
-				}catch(Exception e) {
-					throw new DAOException("DBとの接続の開放に失敗しました");
-					}
-				}
-		}
-
-	public MemberBean findByEail(String email) throws DAOException{
-
-		if(con == null)
-			getConnection();
-
-		PreparedStatement st = null;
-		ResultSet rs = null;
-
-		try {
-			//emailが一致する会員の検索
-			String sql = "SELECT * FROM member WHERE email = ? AND leave_day is null";
-			//stオブジェクトの取得
-			st = con.prepareStatement(sql);
-			//Emailの設定
-			st.setString(1, email);
-			//SQLの実行
-			rs = st.executeQuery();
-			//結果の取得と表示
-			if (rs.next()) {
-				int userId = rs.getInt("user_id");
-				String userName = rs.getString("user_name");
-				String address = rs.getString("address");
-				String tel = rs.getString("tel");
-				email = rs.getString("email");
-				Date birth = rs.getDate("birthday");
-				Date enterDay = rs.getDate("enter_day");
-				Date leaveDay = rs.getDate("leave_day");
-				MemberBean bean = new MemberBean(userId, userName, address, tel, email, birth, enterDay, leaveDay);
-				//会員情報を返す
-				return bean;
-			}
-			//Email該当なし
-			else {
-				return null;
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			throw new DAOException("検索に失敗しました。");
-		}
-		finally {
-			try {
-				if(rs != null) rs.close();
-				if(st != null) st.close();
-				close();
-			}
-			catch (Exception e) {
-				throw new DAOException("リソースの開放に失敗しました。");
-			}
-		}
-	}
-
-	private void getConnection() throws DAOException{
+	private void getConnetion() throws DAOException{
 		try {
 			Class.forName("org.postgresql.Driver");
 			String url ="jdbc:postgresql:library_system";
