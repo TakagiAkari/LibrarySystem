a352a7ae6b12f7c5215daea7692f7a28b8ae4925
244 <2021java52-student006@la-study.com>
2021-06-21 10:23:27 +0900

    貸出のBean

--- src/ls/bean/LendingBean.java
+++ src/ls/bean/LendingBean.java
@@ -4,19 +4,33 @@ import java.io.Serializable;
 
 public class LendingBean implements Serializable {
 
+	private String memo;
 	private String bookId;
 	private String bookName;
 	private String userId;
 	private String userName;
+	private int userIdInt;
+	private int bookIdInt;
 
 	public LendingBean() {}
 
-	public LendingBean(String bookId, String bookName, String userId, String userName) {
+	public LendingBean(String memo, String bookId, String bookName, String userId, String userName, int userIdInt, int bookIdInt) {
 		super();
+		this.memo = memo;
 		this.bookId = bookId;
 		this.bookName = bookName;
 		this.userId = userId;
 		this.userName = userName;
+		this.userIdInt = userIdInt;
+		this.bookIdInt = bookIdInt;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
 	}
 
 	public String getBookId() {
@@ -51,4 +65,19 @@ public class LendingBean implements Serializable {
 		this.userName = userName;
 	}
 
+	public int getUserIdInt() {
+		return userIdInt;
+	}
+
+	public void setUserIdInt(int userIdInt) {
+		this.userIdInt = userIdInt;
+	}
+
+	public int getBookIdInt() {
+		return bookIdInt;
+	}
+
+	public void setBookIdInt(int bookIdInt) {
+		this.bookIdInt = bookIdInt;
+	}
 }
