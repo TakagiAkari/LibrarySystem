15751944cd5f175201331337f47c835fbe5aee43
Rina Hosokubo <2021java52-student020@la-study.com>
2021-06-21 13:26:45 +0900

    src/ls/servlet/ChangeMemberInfoServlet.java更新しました

--- src/ls/servlet/ChangeMemberInfoServlet.java
+++ src/ls/servlet/ChangeMemberInfoServlet.java
@@ -1,8 +1,8 @@
 package ls.servlet;
 
 import java.io.IOException;
-import java.lang.reflect.Member;
-import java.util.List;
+import java.sql.Date;
+import java.text.ParseException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,10 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import ls.bean.InputMemberBean;
 import ls.bean.MemberBean;
 import ls.dao.DAOException;
 import ls.dao.MemberDAO;
-
+import ls.module.OperateDate;
 /**
  * Servlet implementation class ChangeMenberInfoServlet
  */
@@ -35,69 +36,75 @@ public class ChangeMemberInfoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			request.setCharacterEncoding("UTF-8");
-			HttpSession session = request.getSession(false);
-			if (session == null) {
-				request.setAttribute("message", "正しく入力してください");
-				gotoPage(request, response, "/errMessage.jsp");
-				return;
-			}
-			MemberBean bean = (MemberBean) session.getAttribute("member");
-
-
-			try {
-				//パラメータの取得
-				String action = request.getParameter("action");
-
-				if (action == null || action.length() == 0 || action.equals("input_userId")) {
-					gotoPage(request, response, "/inputMemID.jsp");
-				//会員idを入力して変更をクリックで変更情報入力画面へいく
-				} else if(action.equals("input")) {
-					MemberDAO dao = new MemberDAO();
-					int userId = Integer.parseInt(request.getParameter("userId"));
-					dao.findById//名前だけ考える
-					HttpSession sessinnputForChangeMemberServlet = request.getSession(false);
-					session.setAttribute("Member","name");
-					gotoPage(request, response, "/changeMemAlt.jsp");
-					//showをクリックで変更前の情報表示を行う
-				} else if (action.equals("show")) {
-					MemberBean bean = new MemberBean();
-					bean.setUserName(request.getParameter("name"));
-					bean.setAddress(request.getParameter("address"));
-					bean.setTel(request.getParameter("tel"));
-					bean.setEmail(request.getParameter("email"));
-					bean.setBirth(request.getParameter("birth"));
-					gotoPage(request, response, "/checkMemInfo.jsp");
-					// changeは変更確定
-				}else if (action.equals("change")) {
-					MemberBean member = (MemberBean) session.getAttribute("member");
-					if (member == null) { //利用者情報がない
-						request.setAttribute("message", "正しく操作してください");
-						gotoPage(request, response, "/errMember.jsp");
-					}
-					OrderDAO dao = new OrderDAO();
-					String userName = member.saveMember("userName");
-					String addres  = member,saveMember("address");
-					String tel = member.saveMember("tel");
-					String email = member.saveMember("email");
-					Date birth = member.saveMember("birth");
-					//変更後はセッション情報をクリアにする
-					session.removeAttribute("member");
-					//変更情報をユーザに送る
-					request.setAttribute("message", "変更");
-					gotoPage(request, response, "/complete.jsp");
-					request.setAttribute("message", "正しく操作してください");
-					gotoPage(request, response, "/errMessage.jsp");
-				}
-				} catch (DAOException e) {
-					e.printStackTrace();
-					request.setAttribute("message", "内部エラーが発生しました");
-					gotoPage(request, response, "/errMember.jsp");
-				}
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			request.setAttribute("message", "正しく入力してください");
+			gotoPage(request, response, "/errMessage.jsp");
+			return;
 		}
 
+		try {
+			//パラメータの取得
+			String action = request.getParameter("action");
 
+			if (action == null || action.length() == 0 || action.equals("input_userId")) {
+				gotoPage(request, response, "/inputMemID.jsp");
+			//会員idを入力して変更をクリックで変更情報入力画面へいく
+			} else if(action.equals("input")) {
+				MemberDAO dao = new MemberDAO();
+				int userId = Integer.parseInt(request.getParameter("MemID"));
+				MemberBean bean =dao.findMemberByUserID(userId);
+				session.setAttribute("PreviousMemberInfo",bean);
+				gotoPage(request, response, "/changeMemInfo.jsp");
+					//showをクリックで変更前の情報表示を行う
+			} else if (action.equals("show")) {
+				String name = request.getParameter("name");
+				String address = request.getParameter("address");
+				String tel = request.getParameter("tel");
+				String email = request.getParameter("email");
+				int birthY = Integer.parseInt(request.getParameter("birthY"));
+				int birthM = Integer.parseInt(request.getParameter("birthM"));
+				int birthD = Integer.parseInt(request.getParameter("birthD"));
 
+				//birthY,birthM,birthDの型変換
+				session.getAttribute("PreviousMemberInfo");
+				MemberBean ChangeMember = (MemberBean)session.getAttribute("PreviousMemberInfo");
+		try {
+					Date BirthDay = OperateDate.getJavaSqlDateOfYMD(birthY,birthM,birthD);
+					int userId = ChangeMember.getUserId();
+					MemberBean LaterMemberInfo = new MemberBean(userId,name, address, tel, email, BirthDay);
+					session.setAttribute("LaterMemberInfo", LaterMemberInfo);
+					MemberDAO dao = new MemberDAO();
+					dao.ChangeMember(LaterMemberInfo);
+					gotoPage(request, response, "/checkMemAlt.jsp");
+			} catch (ParseException e) {
+					e.printStackTrace();
+					request.setAttribute("message", "不正な日付です");
+					gotoPage(request, response, "/errMessage.jsp");
+				}
+
+			// changeは変更確定
+			}else if (action.equals("change")&&(session != null)) {
+				InputMemberBean memberInfo = (InputMemberBean)session.getAttribute("InputMemberInfo");
+				MemberBean member = (MemberBean) session.getAttribute("PreviousMemberInfo");
+			if (member == null) { //利用者情報がない
+				request.setAttribute("message", "正しく操作してください");
+				gotoPage(request, response, "/errMessage.jsp");
+				return;
+				}
+				//変更後はセッション情報をクリアにする
+				session.removeAttribute("PreviousMemberInfo");
+				//変更情報をユーザに送る
+				request.setAttribute("message", "変更");
+				gotoPage(request, response, "/complete.jsp");
+			}
+		    } catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました");
+			gotoPage(request, response, "/errMessage.jsp");
+		    }
+	    }
 
 	private void gotoPage(HttpServletRequest request,
 			HttpServletResponse response, String page) throws ServletException,
