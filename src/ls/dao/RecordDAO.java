1a21d133f6627902b18c0b3f4a4b308309cd5e10
Takagi Akari <2021java52-student009@la-study.com>
2021-06-21 14:09:01 +0900

    add RecordDAO

--- src/ls/dao/RecordDAO.java
+++ src/ls/dao/RecordDAO.java
@@ -1,7 +1,12 @@
 package ls.dao;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import ls.bean.RecordBean;
 
 //RecordBeanの実装
 
@@ -31,6 +36,56 @@ public class RecordDAO {
 			con.close();
 			con = null;
 		}
+	}
+	public RecordBean findRecordByBookID(int bookID) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		 try {
+			//SQL
+			String sql = "SELECT * FROM record, WHERE book_id = ?";
+			st = con.prepareStatement(sql);
+			//SQLをデータベースへ
+			st.setInt(1, bookID);
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+				int bookId = rs.getInt("book_id");
+				Long isbn = rs.getLong("isbn");
+				Date stockDay = rs.getDate("stock_day");
+				Date throwoutDay =rs.getDate("throwout_day");
+				String memo = rs.getString("memo");
+
+				RecordBean rb = new RecordBean(bookId,isbn, stockDay, throwoutDay,memo);
+				return rb;
+			}
+		} catch (Exception e) {
+					e.printStackTrace();
+		  }
+		return null;
+	   }
+	public void updateThrowoutDay(int bookId) {
+		PreparedStatement st = null;
+		 try {
+			String sql = "UPDATE record SET throwout_day = CURRENT_DATE WHERE book_id = ?";
+			st = con.prepareStatement(sql);
+			st.setInt(1, bookId);
+			st.executeUpdate();
+
+			return;
+		}
+		 catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(st != null) st.close();
+				close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	/*機能実装原型
